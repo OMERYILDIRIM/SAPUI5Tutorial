4510d72c41fa6bf4116efcd0b772c79f9fa25a4f
Manifold <mairead.manifold@sap.com>
2015-12-30 13:04:22 +0000

    Changed popup to a SAPUI5 sap.m MessageToast facility: nicer looking

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,11 +1,12 @@
 sap.ui.define([
-   "sap/ui/core/mvc/Controller"
-], function (Controller) {
+   "sap/ui/core/mvc/Controller",
+   "sap/m/MessageToast"
+], function (Controller, MessageToast) {
    "use strict";
    return Controller.extend("sap.ui.demo.wt.controller.App", {
 	      onShowHello : function () {
-	          // show a native JavaScript alert
-	          alert("Hello World step 5 - Controllers");
+	          // show a SAPUI5 message alert - nicer looking
+	    	  MessageToast.show("Hello World Step 6");
 	       }
 	    });
 });
